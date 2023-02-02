d02e824b4d54216912ea1aad5f7342d634e59b50
GkChris <gkagkaschristos@gmail.com>
2023-02-02 12:29:16 +0200

    cache fix

--- src/api/middleware/Cache.js
+++ src/api/middleware/Cache.js
@@ -7,8 +7,9 @@ module.exports = (req, res, next) => {
     if (req.method != 'GET') cache.clear()
 
     if ( cache.get(req.originalUrl) ) {
-        res.locals.message = statusCodes.ok.msg;
-        return res.status(statusCodes.ok.code).send(cache.get(req.originalUrl))
+        res.locals.message = `${statusCodes.ok.msg} | Send by cache`; 
+        res.status(statusCodes.ok.code).send(cache.get(req.originalUrl))
+        return;
     } 
     else next()
   };
