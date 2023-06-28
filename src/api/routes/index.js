ffb25f63e6cfcc1fe5b1f61d0f537d40ab612d08
GkChris <gkagkaschristos@gmail.com>
2023-06-28 15:26:19 +0300

    Test push

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,4 +4,5 @@ var router = express.Router();
 router.use('/auth', require('../controllers/AuthController'));      
 router.use('/temp', require('../controllers/TempController'));      
 
+
 module.exports = router;
