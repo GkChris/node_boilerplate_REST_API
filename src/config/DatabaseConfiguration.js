070bbbfde6cc83176fe12cfef64d5b472298433c
GkChris <gkagkaschristos@gmail.com>
2023-08-12 19:01:45 +0300

    Made db connection more flexible

--- src/config/DatabaseConfiguration.js
+++ src/config/DatabaseConfiguration.js
@@ -1,14 +1,24 @@
 'use strict';
 const mongoose = require('mongoose');
 
-const db = process.env.DB || 'mongodb://127.0.0.1:27017/mvcboiler'
+const username = 'insert_username';
+const password = 'insert_password';
+const dbName = 'mvcboiler'; 
+const dbHost = '127.0.0.1';
+const dbPort = '27017';
+
+const db = `mongodb://${username}:${password}@${dbHost}:${dbPort}/${dbName}`;
 
 const TestSchema = require('../api/models/Test');
 
-
-
 // Db connection and setup
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
 mongoose.Promise = Promise;
 
 module.exports.database = db;
