8a73bfa9952f95f8725c170c5de0c6dde2200f0d
GkChris <gkagkaschristos@gmail.com>
2024-01-28 01:18:07 +0200

    Fixed typo

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,4 +1,4 @@
-const CommonServices = require('./CommonlServices');
+const CommonServices = require('./CommonServices');
 const AuthService = require('./AuthService');
 const TempService = require('./TempService');
 
